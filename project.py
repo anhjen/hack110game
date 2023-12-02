b513cea7e6edd8cca32a3f483a75342d47b6ff12
Jenny Nguyen <anhjen@ad.unc.edu>
2023-12-02 02:01:31 -0500

    slay

--- project.py
+++ project.py
@@ -4,10 +4,11 @@ import pygame, sys
 
 pygame.init()
 
-size = width, height = 1200, 800
+size = width, height = 1400, 750
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Hunger Games")
+
 
 running = True
 while running:
@@ -16,16 +17,32 @@ while running:
             running = False
     pygame.display.update()
 
-    image_path = "VillageBG.png"
-    image = pygame.image.load(image_path)
-    image_rect = image.get_rect()
+    image_path = "assets/FirstScene.png"
+    og_back = pygame.image.load(image_path)
+    background = pygame.transform.scale(og_back, (width, height))
+    back_rect = background.get_rect()
+
+    image_path2 = "assets/Shrek_(character).png"
+    og_shrek = pygame.image.load(image_path2)
+    shrek = pygame.transform.scale(og_shrek, (width/8, height/3))
+    shrek_rect = shrek.get_rect()
+    shrek_rect.center = (width/1.6, height/1.4)  
+
+    text_color = (255, 255, 255)
+    font = pygame.font.Font(None, 36)
+    text_surface = font.render("Hello, Pygame!", True, text_color)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill((200, 200, 200)) 
-        screen.blit(image, image_rect)
+        screen.fill((255, 255, 255))
+        screen.blit(background, back_rect)
+        screen.blit(shrek, shrek_rect)
+        text_rect = text_surface.get_rect(center=back_rect.center)
+        screen.blit(text_surface, text_rect)
+
         pygame.display.flip()
+
 pygame.quit()
